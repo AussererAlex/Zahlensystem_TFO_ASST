64071e41b000f390129d6c0ba699fa2b998f0437
AussererAlex <63018989+AussererAlex@users.noreply.github.com>
2020-04-24 11:04:48 +0200

    gitignore

---
+++ Zahlensystem_TFO_ASST/Zahlensystem_TFO_ASST/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zahlensystem_TFO_ASST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
