15ac13dd3e90679a78afb8c444717ea0cad5593e
AussererAlex <63018989+AussererAlex@users.noreply.github.com>
2020-04-24 20:05:02 +0200

    Titel

--- Zahlensystem_TFO_ASST/Zahlensystem_TFO_ASST/Program.cs
+++ Zahlensystem_TFO_ASST/Zahlensystem_TFO_ASST/Program.cs
@@ -9,7 +9,32 @@ namespace Zahlensystem_TFO_ASST
     {
         static void Main(string[] args)
         {
+            Titel("Zahlensystem", ConsoleColor.Green);
+
             Console.ReadKey();
+        }
+        static void Titel(string titel, ConsoleColor textfarbe)
+        {
+            int breite = Console.WindowWidth;
+            int textlen = titel.Length;
+            ConsoleColor aktTextFarbe = Console.ForegroundColor;
+            Console.Clear();
+            Console.ForegroundColor = textfarbe;
+
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("*");
+            }
+            for (int i = 0; i < (breite - textlen) / 2; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(titel);
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("*");
+            }
+            Console.ForegroundColor = aktTextFarbe;
         }
     }
 }
